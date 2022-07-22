71d0c98ed7872cff82552718ecefb353c0ae983e
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2022-07-22 23:52:11 +0300

    restaurantcard implemented

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,11 +1,64 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { ArrowRightIcon } from "react-native-heroicons/solid";
+import RestaurantCard from "./RestaurantCard";
 
 export default function FeaturedRow({ id, title, description }) {
   return (
     <View>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
+      <View className="mt-4 flex-row items-center justify-between px-4">
+        <Text className="font-bold text-lg">{title}</Text>
+        <ArrowRightIcon color="#00CCBB" />
+      </View>
+
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
+
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className="pt-4"
+      >
+        {/* Restaurant card */}
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yu! Sushi"
+          rating={4.5}
+          genre="Japanes"
+          address="123 Main St"
+          short_description="This is a test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yu! Sushi"
+          rating={4.5}
+          genre="Japanes"
+          address="123 Main St"
+          short_description="This is a test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yu! Sushi"
+          rating={4.5}
+          genre="Japanes"
+          address="123 Main St"
+          short_description="This is a test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+      </ScrollView>
     </View>
   );
 }
