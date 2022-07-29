e784ed4356e8ce91dcfcf526e30133ee95f5f324
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2022-07-29 18:51:08 +0300

    add restaurant screen

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
       <TailwindProvider>
         <Stack.Navigator>
           <Stack.Screen name="home" component={HomeScreen} />
+          <Stack.Screen name="restaurant" component={RestaurantScreen} />
         </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
